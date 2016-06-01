0fa34029c3a1db4beeb1047342543c58ae25f141
nmittler <nathanmittler@google.com>
2016-06-01 09:05:07 -0700

    adding RunWith annotations to tests.

--- core/src/test/java/com/google/protobuf/experimental/schema/GenericSchemaFactoryTest.java
+++ core/src/test/java/com/google/protobuf/experimental/schema/GenericSchemaFactoryTest.java
@@ -2,6 +2,10 @@ package com.google.protobuf.experimental.schema;
 
 import com.google.protobuf.experimental.example.PojoMessage;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
 public class GenericSchemaFactoryTest extends AbstractSchemaFactoryTest {
   private static final Schema<PojoMessage> SCHEMA =
           new GenericSchemaFactory().createSchema(PojoMessage.class);
--- core/src/test/java/com/google/protobuf/experimental/schema/HandwrittenSchemaFactoryTest.java
+++ core/src/test/java/com/google/protobuf/experimental/schema/HandwrittenSchemaFactoryTest.java
@@ -5,7 +5,10 @@ import com.google.protobuf.experimental.example.PojoMessage;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class HandwrittenSchemaFactoryTest extends AbstractSchemaFactoryTest {
   private static final Schema<PojoMessage> SCHEMA =
           new HandwrittenSchemaFactory().createSchema(PojoMessage.class);
